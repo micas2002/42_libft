636260d377848a280c093f091d53486929c37b19
micas2002 <miguelbernardinosousa2002@gmail.com>
2023-04-04 18:13:09 +0100

    organized files into folders. added ft_printf

--- sources/ft_printf_u.c
+++ sources/ft_printf_u.c
@@ -1,23 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_printf_u.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/18 15:05:02 by mibernar          #+#    #+#             */
-/*   Updated: 2021/11/16 10:47:43 by mibernar         ###   ########.fr       */
+/*   Created: 2022/01/12 13:04:28 by mibernar          #+#    #+#             */
+/*   Updated: 2023/04/04 17:33:50 by mibernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
+int	ft_printf_u(int args)
 {
-	int	x;
+	int		a;
+	char	*str;
 
-	x = 0;
-	while (s[x] != '\0')
-		x++;
-	return (x);
+	a = args;
+	if (a < 0)
+		a = 4294967296 + a;
+	str = ft_unsigned_itoa(a);
+	a = ft_strlen(str);
+	write (1, str, a);
+	free (str);
+	return (a);
 }
--- sources/ft_strdup.c
+++ sources/ft_strdup.c
@@ -6,33 +6,28 @@
 /*   By: mibernar <mibernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 10:55:40 by mibernar          #+#    #+#             */
-/*   Updated: 2021/11/11 11:42:11 by mibernar         ###   ########.fr       */
+/*   Updated: 2022/09/22 15:14:24 by mibernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	ft_strcpy(char *dst, const char *src)
-{
-	int	x;
-
-	x = 0;
-	while (src[x] != '\0')
-	{
-		dst[x] = src[x];
-		x++;
-	}
-	dst[x] = '\0';
-	return (*dst);
-}
-
 char	*ft_strdup(const char *s1)
 {
 	char	*dest;
+	int		x;
 
-	dest = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
+	if (s1[0] == '\0')
+		return (NULL);
+	dest = malloc(sizeof(char) * (ft_strlen(s1) + 1));
 	if (!dest)
 		return (NULL);
-	ft_strcpy(dest, s1);
+	x = 0;
+	while (s1[x] != '\0')
+	{
+		dest[x] = s1[x];
+		x++;
+	}
+	dest[x] = '\0';
 	return (dest);
 }
